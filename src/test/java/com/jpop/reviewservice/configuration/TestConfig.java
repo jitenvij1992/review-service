304fa4b3b83a4fff59f5ce294cd3a91e6d3bbb28
jitenvij <jitenvij1992@gmail.com>
2019-03-10 22:40:49 +0530

    added test cases

---
+++ src/test/java/com/jpop/reviewservice/configuration/TestConfig.java
@@ -0,0 +1,9 @@
+package com.jpop.reviewservice.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.jpop")
+public class TestConfig {
+}
