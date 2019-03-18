20f255827f50b54f21e75277ca5727b90802ef8d
Jiten Vij <jitenvij1992@gmail.com>
2019-03-18 18:27:49 +0530

    Added eureka config

--- src/main/java/com/jpop/reviewservice/ReviewServiceApplication.java
+++ src/main/java/com/jpop/reviewservice/ReviewServiceApplication.java
@@ -1,19 +1,15 @@
 package com.jpop.reviewservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class ReviewServiceApplication {
 
-    @Autowired
-    private DiscoveryClient discoveryClient;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(ReviewServiceApplication.class, args);
 	}
 
